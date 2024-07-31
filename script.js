f692854b547589b1d09ef968795946fb60bf294a
969hadiahmed@gmail.com <969hadiahmed@gmail.com>
2024-07-31 15:58:03 +0530

    Search Feature added

--- script.js
+++ script.js
@@ -11,63 +11,58 @@ class Contact{
         this.github=github;
         this.adress=adress;
         this.gender=gender;
-        contacts.push(this);
-        addContact(this);//Whenever new Contact is created it is automatically added to the CONTACT LIST
+        contacts.push(this);//adding new contact to contacts
+        addContact(this,".contact-list");//Whenever new Contact is created it is automatically added to the CONTACT LIST
     }
 }
-const addContact=(contact)=>{//function to add new contact
+const appendDiv=(className,content,parent)=>{
+    const element=document.createElement("div");
+    element.className=className;
+    element.innerHTML=content;
+    parent.appendChild(element);
+}
+const addContact=(contact,container)=>{//function to add new contact to said Container
     let card=document.createElement("div");//creating contact card
     card.className="contactCard";
-    // Creating Elements to put inside card
-    let contact_name=document.createElement("div");
-    let phno1=document.createElement("div");
-    let phno2=document.createElement("div");
-    let emailID=document.createElement("div");
-    let instaID=document.createElement("div");
-    let fbID=document.createElement("div");
-    let github=document.createElement("div");
-    let adress=document.createElement("div");
-    let dob=document.createElement("div");
-    let gender=document.createElement("div");
-    // Assigning class to Elements
-    contact_name.className="contact_name";
-    phno1.className="phno1";
-    phno2.className="phno2";
-    emailID.className="emailID";
-    instaID.className="instaID";
-    fbID.className="fbID";
-    github.className="github";
-    adress.className="adress";
-    dob.className="dob";
-    gender.className="gender";
-    // Assigning text to Elements
-    contact_name.innerHTML="Name:"+contact.contact_name;
-    phno1.innerHTML="Phone Number1:"+contact.phno1;
-    phno2.innerHTML="Phone Number2:"+contact.phno2;
-    emailID.innerHTML="Email ID:"+contact.emailID;
-    instaID.innerHTML="Instagram:"+contact.instaID;
-    fbID.innerHTML="Face Book:"+contact.fbID;
-    github.innerHTML="Github:"+contact.github;
-    adress.innerHTML="Adress:"+contact.adress;
-    dob.innerHTML="DOB:"+contact.dob;
-    gender.innerHTML="Gender:"+contact.gender;
-    // Adding Elements to card
-    card.appendChild(contact_name);
-    card.appendChild(phno1);
-    card.appendChild(phno2);
-    card.appendChild(emailID);
-    card.appendChild(instaID);
-    card.appendChild(fbID);
-    card.appendChild(github);
-    card.appendChild(adress);
-    card.appendChild(dob);
-    card.appendChild(gender);
-    // Adding Card to HTML File
-    $(".contact-list").append(card);
+    appendDiv("contact_name","Name:"+contact.contact_name,card);
+    appendDiv("contact_phno1","Phone Number 1:"+contact.phno1,card);
+    appendDiv("contact_phno2","Phone Number 2:"+contact.phno2,card);
+    appendDiv("contact_email","Email ID:"+contact.emailID,card);
+    appendDiv("contact_insta","Instagram ID:"+contact.instaID,card);
+    appendDiv("contact_fb","Facebook ID:"+contact.fbID,card);
+    appendDiv("contact_git","GitHub ID:"+contact.github,card);
+    appendDiv("contact_adress","Address:"+contact.adress,card);
+    appendDiv("contact_dob","Date of Birth:"+contact.dob,card);
+    appendDiv("contact_gender","Gender:"+contact.gender,card);
+    $(container).append(card);
 }
+function resetForm() {
+    $("#name").val("");
+    $("#phno1").val("");
+    $("#phno2").val("");
+    $("#emailID").val("");
+    $("#instaID").val("");
+    $("#fbID").val("");
+    $("#github").val("");
+    $("#adress").val("");
+    $("#dob").val("");
+    $("#gender").val("");
+}
+//Adding Random Contacts for Testing Purposes
 let new_contact=new Contact("Hadi",123,456,"abc@gmail.com","abc_def","facebook","git","123 street,kolkata700012","1-2-2002","Male");
+new Contact("Qadir Ahmed");
+new Contact("Deep Das")
+new Contact("Arnab Dey")
+new Contact("Atalanta Dey")
+new Contact("Sourav Das")
+new Contact("Harsh Jaiswal")
+new Contact("Raj Jaiswal")
+new Contact("Rajdeep Mondal")
+new Contact("Srijan Mondal")
+new Contact("Mohit Gupta")
+new Contact("Ankit Gupta")
 $(document).ready(()=>{
-    $('#submit').on(
+    $('#submit').on(//adding new Contact
         {"click": function(){//When Button is Clicked
             //Extracting Value From Form
             let contact_name=$("#name").val();
@@ -82,17 +77,18 @@ $(document).ready(()=>{
             let gender=$("#gender").val();
             // Using Extracted Value to Genrate new Object
             new Contact(contact_name,phno1,phno2,emailID,instaID,fbID,github,adress,dob,gender);
-            //Resetting input box
-            $("#name").val("");
-            $("#phno1").val("");
-            $("#phno2").val("");
-            $("#emailID").val("");
-            $("#instaID").val("");
-            $("#fbID").val("");
-            $("#github").val("");
-            $("#adress").val("");
-            $("#dob").val("");
-            $("#gender").val("");
+            resetForm();//Resetting input box
         }}
     )
+    //Search
+    $('#search-btn').on({
+        "click":function(){
+            $(".search-result").children().remove();
+            contacts.forEach(contact => {
+                if(contact.contact_name.toLowerCase().includes($("#search").val().toLowerCase())){
+                    addContact(contact,".search-result");
+                }
+            });
+        }
+    });
 })
