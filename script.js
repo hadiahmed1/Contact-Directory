82fa0b2e07b2e2c203be5db0d8384f089100973c
969hadiahmed@gmail.com <969hadiahmed@gmail.com>
2024-08-04 17:57:08 +0530

    Discover Button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let agency_list=[]//Will be replaced a list of Objects
+let agency_list=[];//Will be replaced a list of Objects
 class Agency{
     constructor(agency_name,phno,emailID,instaID,adress,country,city){
         this.agency_name=agency_name;
@@ -12,12 +12,40 @@ class Agency{
         // addAgency(this,".agency-list");//Whenever new Contact is created it is automatically added to the CONTACT LIST
     }
 }
+let destinations=[];
+class Destination{
+    constructor(city,country,picture){
+        this.city=city;
+        this.country=country;
+        this.picture=picture;
+        createDestinationCard(picture,city,country);
+    }
+}
 const appendDiv=(className,content,parent)=>{
     const element=document.createElement("div");
     element.className=className;
     element.innerHTML=content;
     parent.appendChild(element);
 }
+function createDestinationCard(picture,city,country){
+    let card=document.createElement("li");
+    let img=document.createElement("img")
+    img.setAttribute("src",picture);
+    card.appendChild(img);
+    let p=document.createElement("p");
+    p.innerHTML=`${country} <strong>${city}</strong>`;
+    card.appendChild(p);
+    appendDiv("agency-list","",card);
+    document.body.querySelector("#destinations").appendChild(card);
+}
+new Destination("Tokyo","Japan","./images/places/tokyo.jpg");
+new Destination("Paris","France","./images/places/paris.jpg");
+new Destination("Bali","Indonesia","./images/places/bali.jpg");
+new Destination("Sydney","Australia","./images/places/sydney.jpg");
+new Destination("Hawaii","USA","./images/places/hawaii.jpg");
+new Destination("Munich","Germany","./images/places/munich.jpg");
+new Destination("Miami","USA","./images/places/miami.jpg")
+
 const addAgency=(agency,container)=>{//function to add new contact to said Container
     let card=document.createElement("div");//creating contact card
     card.className="contactCard";
@@ -32,13 +60,19 @@ const addAgency=(agency,container)=>{//function to add new contact to said Conta
 }
 function resetForm() {
 }
+function addSearchBar(parent){
+    let search_bar=document.createElement("div");
+    search_bar.className="search_bar";
+    search_bar.innerHTML=`<input type="text" id="search" placeholder="Search..."><button id="search-btn">Search</button><div class="search-result"></div>`;
+    $(parent).append(search_bar);
+}
 //Adding Random Contacts for Testing Purposes
 new Agency("bcd",1234,"","","","USA","miamai");
 new Agency("cde",1234,"","","","USA","new york");
 new Agency("abc",1234,"","","","USA","new york");
 new Agency("def",1234,"","","","USA","new york");
 new Agency("efg",1234,"","","","UK","London");
-new Agency("ghi",1234,"","","","UK","London");
+new Agency("ghi",1234,"","","","Japan","Tokyo");
 new Agency("hij",1234,"","","","UK","London");
 new Agency("ijk",1234,"","","","UK","London");
 new Agency("ijk",1234,"","","","Spain","Barcelona");
@@ -46,14 +80,18 @@ new Agency("jkl",1234,"","","","Spain","Barcelona");
 
 $(document).ready(()=>{
     //Search
-    $('#search-btn').on({
+    $("#hero").on("click", "#search-btn", function(){
+        $(".search-result").children().remove();
+        agency_list.forEach(agency => {
+            console.log(agency.agency_list);
+            if(agency.city.toLowerCase().includes($("#search").val().toLowerCase())){
+                addAgency(agency,".search-result");
+            }
+        });
+    });
+    $("#discover").on({
         "click":function(){
-            $(".search-result").children().remove();
-            agency_list.forEach(agency => {
-                if(agency.agency_name.toLowerCase().includes($("#search").val().toLowerCase())){
-                    addAgency(agency,".search-result");
-                }
-            });
+            addSearchBar($(this).parent());
         }
     });
     $("strong").on({
